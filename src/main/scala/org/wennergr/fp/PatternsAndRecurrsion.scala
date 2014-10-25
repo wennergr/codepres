bfc432648cf470a6332d06d59e9a2d3a5f0960b6
Tobias Wennergren <wennergr@gmail.com>
2014-10-25 12:34:14 -0700

    Added work from tech huddle

--- src/main/scala/org/wennergr/fp/PatternsAndRecurrsion.scala
+++ src/main/scala/org/wennergr/fp/PatternsAndRecurrsion.scala
@@ -1,5 +1,35 @@
 package org.wennnergr.fp
 
-class PatternsAndRecurrsion {
+object PatternsAndRecurrsion {
 
+  def length[T](lst: List[T]): Int = lst match {
+    case Nil      => 0
+    case x :: xs  => 1 + length(xs)
+  }
+
+  def take[T](lst: List[T], n: Int): List[T] = lst match {
+    case x :: xs if n > 0  => x :: take(xs, n - 1)
+    case _ => List()
+  }
+
+// takeWhile(List(1,2,3,4,5,6)( x => x < 4)
+  def takeWhile[T](lst: List[T])(f: T => Boolean): List[T] = {
+    lst match {
+      case x :: xs if f(x) => x :: takeWhile(xs)(f)
+      case _ => List()
+    }
+  }
+
+  def takeWhileNoCurry[T](lst: List[T],f: T => Boolean): List[T] = {
+    lst match {
+      case x :: xs if f(x) => x :: takeWhileNoCurry(xs, f)
+      case _ => List()
+    }
+  }
+
+  def flatten(lst: List[_]): List[_] = lst match {
+    case (x: List[_]) :: xs => flatten(x) ::: flatten(xs)
+    case x :: xs => x :: flatten(xs)
+    case _ => Nil
+  }
 }
--- src/test/scala/org/wennergr/fp/PatternsAndRecurrsionTest.scala
+++ src/test/scala/org/wennergr/fp/PatternsAndRecurrsionTest.scala
@@ -1,8 +1,38 @@
 package org.wennergr.fp
 
-import org.scalatest.{Matchers, FlatSpec, FunSuite}
+import org.scalatest.{FreeSpec, Matchers, FlatSpec, FunSuite}
+import org.wennnergr.fp.PatternsAndRecurrsion
 
 
-class PatternsAndRecurrsionTest extends FlatSpec with Matchers {
+class PatternsAndRecurrsionTest extends FreeSpec with Matchers {
 
+  "the length function" - {
+    "should return the length of a string" in {
+      PatternsAndRecurrsion.length(List(1,2,3,4,5)) should be(5)
+    }
+    "it should return 0 if the list is empty" in {
+      PatternsAndRecurrsion.length(List()) should be (0)
+    }
+
+  }
+
+  "the take function" - {
+    "Should return the first N element from a list" in {
+      PatternsAndRecurrsion.take(List(1,2,3,4),2) should be(List(1,2))
+    }
+  }
+
+  "the takeWhile function" - {
+    "Should return the elements while expression is true" in {
+      PatternsAndRecurrsion.takeWhile(List(1,2,3,4))(x => x < 4) should be(List(1,2,3))
+    }
+  }
+
+  // flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+  // List(1, 1, 2, 3, 5, 8)
+  "the flatten function" - {
+    "Should flatten all the things!" in {
+      PatternsAndRecurrsion.flatten(List(List(1, 1), List(2), List(3, List(5, 8)))) should be (List(1, 1, 2, 3, 5, 8))
+    }
+  }
 }
